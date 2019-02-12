136efaeb94e0656a9bfa9a03de483c503c6f91ec
Phong Truong <phongb1507140@gmail.com>
2019-02-12 14:33:07 +0700

    test

--- src/app.js
+++ src/app.js
@@ -13,61 +13,62 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
     modal.style.display = "none";
 }
-btnCall.onclick = function(){
-    openStream(function (stream) {
-        playVideo(stream, 'LocalStream')
-        const p = new Peer({ initiator: location.hash === '#1', trickle: false, stream });
-    
-        p.on('signal', token => {
-            $('#txtMySignal').val(JSON.stringify(token));
-            console.log(JSON.stringify(token));
-        });
-    
-        $('#btnConnect').click(() => {
-            const friendSignal = JSON.parse($('#txtFriendSignal').val());
-            p.signal(friendSignal);
-        });
-    
-        p.on('stream', stream => playVideo(stream, 'FriendStream'));
+btnCall.onclick = function () {
+    const friendID = $('#txtFriendID').val();
+    openStream(stream => {
+        playVideo(stream, 'LocalStream');
+        const call = peer.call(friendID, stream);
+        call.on('stream', remoteStream => playVideo(remoteStream, 'FriendStream'));
     });
+    // openStream(function (stream) {
+    //     playVideo(stream, 'LocalStream')
+    //     const p = new Peer({ initiator: location.hash === '#1', trickle: false, stream });
+
+    //     p.on('signal', token => {
+    //         $('#txtMySignal').val(JSON.stringify(token));
+    //         console.log(JSON.stringify(token));
+    //     });
+
+    //     $('#btnConnect').click(() => {
+    //         const friendSignal = JSON.parse($('#txtFriendSignal').val());
+    //         p.signal(friendSignal);
+    //     });
+
+    //     p.on('stream', stream => playVideo(stream, 'FriendStream'));
+    // });
 }
 // When the user clicks the button, open the modal 
 btn.onclick = function () {
     modal.style.display = "block";
     var x = window.outerWidth;
-    if(x > 780){
+    if (x > 780) {
         document.getElementById("LocalVideo").style.right = ((x - 780) / 2) + "px";
-    };
-    // const friendID  = $('#txtFriendID').val();
-    // openStream(stream => {
-    //     playVideo(stream, 'LocalStream');
-    //     const call = peer.call(friendID, stream);
-    //     call.on('stream', remoteStream => playVideo(remoteStream, 'FriendStream'));
-    // });
+    }
 }
 const openStream = require('./openStream');
 const playVideo = require('./playVideo');
-const Peer = require('simple-peer');
+// const Peer = require('simple-peer');
+const Peer = require('peerjs');
 const uid = require('uid');
 const $ = require('jquery');
 
-// const config = {host: 'nienluan2019.herokuapp.com', port: 443, secure:true, key: 'peerjs'}
-// function getPeer(){
-//     const id = uid(10);
-//     $('#peer-id').append(id);
-//     return id;
-// }
-// const peer = new Peer(getPeer(), config);
+const config = {host: 'nienluan2019.herokuapp.com', port: 443, secure:true, key: 'peerjs'}
+function getPeer(){
+    const id = uid(10);
+    $('#peer-id').append(id);
+    return id;
+}
+const peer = new Peer(getPeer(), config);
 
 // When the user clicks on <span> (x), close the modal
 
-// peer.on('call', (call)=>{
-//     openStream(stream => {
-//         playVideo(stream, 'LocalStream');
-//         call.answer(stream);
-//         call.on('stream', remoteStream => playVideo(remoteStream, 'FriendStream'));
-//     }); 
-// });
+peer.on('call', (call)=>{
+    openStream(stream => {
+        playVideo(stream, 'LocalStream');
+        call.answer(stream);
+        call.on('stream', remoteStream => playVideo(remoteStream, 'FriendStream'));
+    }); 
+});
 // When the user clicks anywhere outside of the modal, close it
 // window.onclick = function(event) {
 //   if (event.target == modal) {
